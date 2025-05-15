e481e23850bdd582ef9f268608839527d63f25a7
Vanhk <anhkhoanq1611@gmail.com>
2025-05-15 19:24:18 +0700

    App update

--- app/src/main/java/com/example/appdocsachv2/view/activity/HomeActivity.java
+++ app/src/main/java/com/example/appdocsachv2/view/activity/HomeActivity.java
@@ -1,25 +1,82 @@
 package com.example.appdocsachv2.view.activity;
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
-import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.GravityCompat;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.example.appdocsachv2.R;
+import com.google.android.material.navigation.NavigationView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
 
+        // Khởi tạo DrawerLayout và NavigationView
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
 
+        // Thiết lập Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Cấu hình DrawerToggle với Toolbar
+        drawerToggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+
+        // Xử lý sự kiện khi chọn mục trong Navigation Drawer
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.nav_my_books) {
+                    // Xử lý "Sách của tôi"
+                } else if (id == R.id.nav_continue_reading) {
+                    // Xử lý "Đọc tiếp"
+                } else if (id == R.id.nav_favorite_books) {
+                    // Xử lý "Sách yêu thích"
+                } else if (id == R.id.nav_export) {
+                    // Xử lý "Đăng xuất"
+                } else if (id == R.id.nav_settings) {
+                    // Xử lý "Cài đặt"
+                } else if (id == R.id.nav_about) {
+                    // Xử lý "About"
+                }
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
